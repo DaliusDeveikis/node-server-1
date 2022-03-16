446f8f28202956dd0edd82ffd875fb3e10bffa0e
DaliusDeveikis <slkyper@gmail.com>
2022-03-16 21:21:40 +0200

    project skeleton

---
+++ index.js
@@ -0,0 +1,22 @@
+export const app = {};
+
+app.init = () => {
+  // ready for new folders
+
+  // ready for new files
+
+  // connect to mysql
+
+  // start server
+
+  console.log('Server is running...');
+
+  // launching regular processes:
+  // -- delete old/unnecessary files
+  // -- less use of file archiving
+  // -- recoverable information via API
+};
+
+app.init();
+
+console.log(app);

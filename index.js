082186b40ed24cf39e246ccceac966e032ac708e
DaliusDeveikis <slkyper@gmail.com>
2022-03-31 17:49:03 +0300

    Update

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ import { server } from './lib/server.js';
 const app = {};
 
 app.init = () => {
-    // pasiruosti pradinius folder'ius
+  // pasiruosti pradinius folder'ius
 
-    // pasiruosti pradinius failus
+  // pasiruosti pradinius failus
 
-    // prisijungimas prie DB (duomenu baze)
+  // prisijungimas prie DB (duomenu baze)
 
-    // uzkurti pati serveri (musu programa)
-    server.init();
+  // uzkurti pati serveri (musu programa)
+  server.init();
 
-    // reguliariu procesu paleidimas:
-    // - istrinti senus/nebereikalingus failus
-    // - maziau naudojamu failu archivavimas
-    // - atsinaujinti informacija per/is API
-}
+  // reguliariu procesu paleidimas:
+  // - istrinti senus/nebereikalingus failus
+  // - maziau naudojamu failu archivavimas
+  // - atsinaujinti informacija per/is API
+};
 
 app.init();
 

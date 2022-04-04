e86a8763bcfbe3ea50c5fdee797cf635109b48ad
DaliusDeveikis <slkyper@gmail.com>
2022-04-04 21:16:32 +0300

    initial form data object

---
+++ public/js/pages/auth.js
@@ -0,0 +1,28 @@
+console.log(1234);
+
+/*
+1) Susirasti forma ir jos Visus laukus
+2) Surinkti informacija is formos
+3) FE validacija
+4-a) Jei yra klaidu - atvaizduojame
+4-b) Jei nera klaidu -siunciame i serveri.
+5) Is serverio gauta atsakima interpretuojame ir kazka darome
+6-a) jei serveris rado kalidu -atvaizduojame
+6-b) jei serveris ne rado klaidu --End
+*/
+
+const formDOM = document.querySelector('.form');
+const errorsDOM = document.querySelector('.form-errors');
+const allInputDOM = document.querySelectorAll('input');
+const submitDOM = document.querySelector('button');
+
+submitDOM.addEventListener('click', e => {
+  e.preventDefault();
+
+  const formData = {};
+  for (const inputDOM of allInputDOM) {
+    const { id, value } = inputDOM;
+    formData[id] = value;
+  }
+  console.log(formData);
+});

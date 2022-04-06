095ef920f04a3bc3c38e97b2cbc3c560a3d91e7b
DaliusDeveikis <slkyper@gmail.com>
2022-04-06 20:13:27 +0300

    server response data acount rout

---
+++ api/account.js
@@ -0,0 +1,17 @@
+const handler = {};
+
+handler.account = (data, callback) => {
+  const acceptableMethods = ['get', 'post', 'put', 'delete'];
+
+  if (acceptableMethods.includes(data.httpMethod)) {
+    return callback(200, {
+      status: 'OK',
+      msg: 'Account: veiksmas leistinas',
+      time: Date.now()
+    });
+  } else {
+    return callback(400, 'Account: veiksmas NEleistinas');
+  }
+};
+
+export default handler;

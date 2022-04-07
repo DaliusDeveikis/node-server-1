a29f149aeda495b7bf7d0f07a11e34caa5d94981
DaliusDeveikis <slkyper@gmail.com>
2022-04-07 19:36:52 +0300

    readsaccount list

--- api/account.js
+++ api/account.js
@@ -1,17 +1,88 @@
+import { IsValid } from '../lib/IsValid.js';
+import { file } from '../lib/file.js';
+
 const handler = {};
 
-handler.account = (data, callback) => {
+handler.account = async (data, callback) => {
   const acceptableMethods = ['get', 'post', 'put', 'delete'];
 
   if (acceptableMethods.includes(data.httpMethod)) {
-    return callback(200, {
-      status: 'OK',
-      msg: 'Account: veiksmas leistinas',
-      time: Date.now()
-    });
-  } else {
-    return callback(400, 'Account: veiksmas NEleistinas');
+    return await handler._method[data.httpMethod](data, callback);
   }
+
+  return callback(400, 'Account: veiksmas NEleistinas');
+};
+
+handler._method = {};
+
+handler._method.post = async (data, callback) => {
+  const minUsernameLength = 4;
+  const maxUsernameLength = 20;
+
+  // 1) reikia patikrinti ar data.payload (keys and values) yra teisingi
+  const user = data.payload;
+  if (typeof user !== 'object' || Object.keys(user).length !== 3) {
+    return callback(200, {
+      status: 'Error',
+      msg: 'Vartotojo objekta sudaro tik 3 elementai (username, email, password)'
+    });
+  }
+  const accountList = await file.list('accounts');
+  console.log(accountList);
+
+  const [usernameError, usernameMsg] = IsValid.username(user.username);
+  if (usernameError) {
+    return callback(200, {
+      status: 'Error',
+      msg: usernameMsg
+    });
+  }
+  const [emailError, emailMsg] = IsValid.email(user.email);
+  if (emailError) {
+    return callback(200, {
+      status: 'Error',
+      msg: emailMsg
+    });
+  }
+  const [passwordError, passwordMsg] = IsValid.password(user.password);
+  if (passwordError) {
+    return callback(200, {
+      status: 'Error',
+      msg: passwordMsg
+    });
+  }
+
+  // 2) nuskaitome kokie failai yra .data/accounts folderyje
+  // 3) patikrinti ar nera failo [email].json (jau sukurtas account'as)
+  // 4) uzsifruoti vartotojo slaptazodi
+  // 5) sukuriame [email].json ir i ji irasome vartotojo objekta
+
+  console.log(data.payload);
+  return callback(200, {
+    action: 'POST',
+    msg: 'Vartotojo paskyra sukurta sekmingai'
+  });
+};
+
+handler._method.get = (data, callback) => {
+  return callback(200, {
+    action: 'GET',
+    msg: 'Stai tau visa info apie dominanti vartotoja'
+  });
+};
+
+handler._method.put = (data, callback) => {
+  return callback(200, {
+    action: 'PUT',
+    msg: 'Vartotojo informacija sekmingai atnaujinta'
+  });
+};
+
+handler._method.delete = (data, callback) => {
+  return callback(200, {
+    action: 'DELETE',
+    msg: 'Vartotojas sekmingai istrinta is sistemos'
+  });
 };
 
 export default handler;
